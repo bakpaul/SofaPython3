106c105c789933e64bd1b9031760dbd4fda512f1
bakpaul <bakpaul@hotmail.fr>
2024-04-25 17:31:50 +0200

    WIP POC

--- splib/core/mappedObject.py
+++ splib/core/mappedObject.py
@@ -1,6 +1,4 @@
 
-
-
 def addMappedObject(node,_mapping="BarycentricMapping",_template="Vec3"):
     node.addObject("MechanicalObject", name='mstate',template=_template)
     node.addObject("BarycentricMapping")
--- splib/core/simulatedObject.py
+++ splib/core/simulatedObject.py
@@ -1,14 +1,17 @@
-def _addDefaultImplicitSolvers(node, iterative=False,_iterations=25,_tolerance=1e-5,_threshold=1e-5):
-    node.addObject('EulerImplicitSolver', name='TimeIntegrationSchema')
+
+class SimulatedObject():
+    def __init__(self, parentNode, _name, _template):
+        self.node = parentNode.addChild(_name)
+        self.node.addObject("MechanicalObject", name='MState',template=_template)
+
+
+
+
+def addDefaultImplicitSolvers(Obj:SimulatedObject, iterative=False,_iterations=25,_tolerance=1e-5,_threshold=1e-5):
+    Obj.node.addObject('EulerImplicitSolver', name='ODESolver')
     if iterative:
-        node.addObject('CGLinearSolver', name='LinearSolver', iterations=_iterations, tolerance=_tolerance, threshold=_threshold)
+        Obj.node.addObject('CGLinearSolver', name='LinearSolver', iterations=_iterations, tolerance=_tolerance, threshold=_threshold)
+    else:
+        Obj.node.addObject('SparseLDLSolver', name='LinearSolver', template='CompressedRowSparseMatrix')
 
-    node.addObject('SparseLDLSolver', name='LinearSolver', template='CompressedRowSparseMatrix')
-    return node
 
-
-def addSimulatedObject(node,_name,_addSolver=_addDefaultImplicitSolvers,_template="Vec3"):
-    simu = node.addChild(_name)
-    _addSolver(simu)
-    simu.addObject("MechanicalObject", name='mstate',template=_template)
-    return node
