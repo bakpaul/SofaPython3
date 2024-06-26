562b8a506f82774e50744e06ab369965ad766ac7
bakpaul <bakpaul@hotmail.fr>
2024-06-26 11:44:02 +0200

    Fix conflicts

--- splib/modeler/ModeledObject.py
+++ splib/modeler/ModeledObject.py
@@ -17,7 +17,7 @@ class InstanciationMethod():
         self.m_params = _params
     def __call__(self,
                  node):
-        self.m_method(node,self.m_params)
+        self.m_method(node,**self.m_params)
     def setParams(self,_params:Dict):
         self.m_params = _params
 
@@ -27,32 +27,32 @@ class InstanciationMethod():
 ##########
 class ModeledObject(object):
     class MethodList():
-        instanciationMethods : List[InstanciationMethod]
+        m_methods : List[InstanciationMethod]
 
         def __init__(self):
-            self.instanciationMethods = []
+            self.m_methods = []
         def __iter__(self):
             self._iterID = -1;
             return self
 
         def __next__(self):
-            if self._iterID + 1  < len(self.instanciationMethods):
+            if self._iterID + 1  < len(self.m_methods):
                 self._iterID += 1
-                return self.instanciationMethods[self._iterID]
+                return self.m_methods[self._iterID]
             else:
                 raise StopIteration
 
         def append(self,_method : InstanciationMethod):
-            for met in self.instanciationMethods:
+            for met in self.m_methods:
                 if met.m_methodID == _method.m_method:
                     print('Method with same id already exists in the list')
                     return UserWarning
-            self.instanciationMethods.append(_method)
+            self.m_methods.append(_method)
 
         def find(self, _methodID:str) -> int:
             id = -1
             acc = -1
-            for met in self.instanciationMethods:
+            for met in self.m_methods:
                 acc += 1
                 if met.m_methodID == _methodID:
                     id = acc
@@ -61,27 +61,32 @@ class ModeledObject(object):
         def get(self, _methodID:str) :
             id = self.find(_methodID)
             if id != -1:
-                return self.instanciationMethods[id]
+                return self.m_methods[id]
             else:
                 return RuntimeError
 
         def remove(self, _methodID:str) -> int:
             id = self.find(_methodID)
             if id != -1:
-                self.instanciationMethods.pop(id)
+                self.m_methods.pop(id)
             return id
 
 
-    instanciationMethods : MethodList
+    m_instanciationMethods : MethodList
     name                 : str
     def __init__(self,_name):
         self.name = _name
         self.instanciationMethods = ModeledObject.MethodList()
+
     def instantiate(self, _node):
         self.node = _node.addChild(self.name)
-        for tup in self.instanciationMethods:
+        for tup in self.m_instanciationMethods:
            tup(self.node)
 
+    def printDebug(self):
+        print(self.name)
+        for tup in self.m_instanciationMethods:
+            print("  " + tup.m_methodID)
 
 
 
@@ -100,35 +105,34 @@ class SimulatedObject(ModeledObject):
         self.addMecha(_mechaParams)
 
     def addObject(self,_objectType:str, _name:str,**kwargs):
-        self.instanciationMethods.append(InstanciationMethod("AddObject(" +_objectType + ":" + _name + ")" ,SimulatedObject._implAddObject,{"_objectType" : _objectType, "_name" : _name, **kwargs}))
+        self.m_instanciationMethods.append(InstanciationMethod("AddObject(" +_objectType + ":" + _name + ")" ,SimulatedObject._implAddObject,{"_objectType" : _objectType, "_name" : _name, **kwargs}))
 
     def addSolvers(self,**kwargs):
-        self.instanciationMethods.append(InstanciationMethod("Solvers",SimulatedObject._implAddSolvers,{**kwargs}))
-
+        self.m_instanciationMethods.append(InstanciationMethod("Solvers",SimulatedObject._implAddSolvers,kwargs))
 
     def addTopology(self,some,args,**kwargs):
-        self.instanciationMethods.append(InstanciationMethod("Topology",SimulatedObject._implAddTopology,{"some" : some, "args" : args, **kwargs}))
+        self.m_instanciationMethods.append(InstanciationMethod("Topology",SimulatedObject._implAddTopology,{"some" : some, "args" : args, **kwargs}))
 
     def addMecha(self,some,args,**kwargs):
-        self.instanciationMethods.append(InstanciationMethod("Mecha",SimulatedObject._implAddMecha,{"some" : some, "args" : args, **kwargs}))
+        self.m_instanciationMethods.append(InstanciationMethod("Mecha",SimulatedObject._implAddMecha,{"some" : some, "args" : args, **kwargs}))
 
     def addCollisionModel(self,some,args,**kwargs):
-        self.instanciationMethods.append(InstanciationMethod("Collision",SimulatedObject._implAddCollisionModel,{"some" : some, "args" : args, kwargs : kwargs}))
+        self.m_instanciationMethods.append(InstanciationMethod("Collision",SimulatedObject._implAddCollisionModel,{"some" : some, "args" : args, kwargs : kwargs}))
 
     def addMappedObject(self,
                         obj:ModeledObject,
                         mappingType:str = "Barycentric"):
-        self.instanciationMethods.append(InstanciationMethod(obj.name,SimulatedObject._implAddMappedObjects,{ "obj" : obj ,"mappingType" : mappingType }))
+        self.m_instanciationMethods.append(InstanciationMethod("Map " + obj.name,SimulatedObject._implAddMappedObjects,{ "obj" : obj ,"mappingType" : mappingType }))
 
     @staticmethod
     def _implAddObject(node, _objectType:str, _name:str, **kwargs) -> None:
         node.addObject(_objectType,name = _name,**kwargs)
     @staticmethod
     def _implAddSolvers(node, odeType:str = "EulerImplicit", linearSolverType:str="SparseLDLSolver",**kwargs) -> None:
-        # node.addObject(odeType,kwargs["ODEParams"])
-        # node.addObject(linearSolverType,kwargs["LinearSolverParams"])
-        node.addObject(odeType,name="tata")
-        node.addObject(linearSolverType,name="toto")
+        node.addObject(odeType,**(kwargs['ODEParams']))
+        node.addObject(linearSolverType,**(kwargs['LinearSolverParams']))
+        # node.addObject(odeType,name="tata")
+        # node.addObject(linearSolverType,name="toto")
 
     @staticmethod
     def _implAddTopology(node, elemType:str = "points", linearSolverType:str="SparseLDLSolver",**kwargs) -> None:
@@ -155,28 +159,39 @@ class displayNode():
         self.prefix = ""
         for i in range(_level):
             self.prefix += "|"
-    def addObject(self,type:str,**kwargs):
-        print(self.prefix + type)
 
+    def addObject(self,type:str,**kwargs):
+        print(self.prefix + type + " with " + str(kwargs))
 
     def addChild(self,name:str):
         print(self.prefix + "Node : " + name)
         return displayNode(len(self.prefix) + 1)
 
 
-
 if __name__ == "__main__":
     Obj1 = SimulatedObject("Obj1")
     Obj2 = SimulatedObject("Obj2")
 
-    Obj1.addSolvers()
-    Obj1.addObject("TetrahedronFEMForceField","ff")
+    # Root = RootNode()
 
-    Obj2.addSolvers()
+    Obj1.addSolvers(odeType="EulerImplicitHugo",ODEParams = { "some" : 1, "arg" : 3}, LinearSolverParams = { "other" : 1, "args" : 3})
+    Obj1.addObject("MyAwesomeComponent","ff")
 
+    Obj2.addSolvers(ODEParams = { "some" : 12, "arg" : 32}, LinearSolverParams = { "other" : 781, "args" : 7853})
     Obj1.addMappedObject(Obj2,"Barycentric")
+
+    # Root.addSubNode(Obj1)
+    # Root.addSimulateObject(param1 = 12, params2 = 23)
 
     DN = displayNode()
     Obj1.instantiate(DN)
+
+
+    print("")
+    Obj1.printDebug()
+    print("")
+    Obj2.printDebug()
+
+
 
 
