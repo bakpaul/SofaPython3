6598e3914ae80a01a7d80d8bd3a8510eb1f5a014
Paul Baksic <paul.baksic@outlook.fr>
2024-07-01 12:10:16 +0200

    Ideas...

--- splib/prefabs/simulated_object.py
+++ splib/prefabs/simulated_object.py
@@ -1,10 +1,23 @@
 from core.node_wrapper import BasePrefab
 
 class SimulatedObject(BasePrefab):
-    def __init__(self,node):
-        self.node = node
-        self.type = "SimulatedObject"
+    def __init__(self,node, _template, _elemType, _filename=None,_source=None,*args,**kwargs):
+        super().__init__(node,*args,**kwargs)
 
-    def __getattr__(self, item):
-        return getattr(self.node,item)
+        self.template=_template
 
+    def addConstitutiveModel(self):
+        ##ADD selasticity
+        ## ADD Mass
+        return
+
+    def addDirichletConditions(self):
+        ##ADD selasticity
+        ## ADD Mass
+        return
+
+    def addVisualModel(self):
+        return
+
+    def addCollisionModel(self):
+        return
