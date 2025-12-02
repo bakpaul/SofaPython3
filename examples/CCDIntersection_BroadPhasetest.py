14da924ed5018b246957fa0af2511001c14a84e9
Paul Baksic <paul.baksic@outlook.fr>
2025-12-02 17:22:44 +0100

    Add tests scene

--- examples/CCDIntersection_BroadPhasetest.py
+++ examples/CCDIntersection_BroadPhasetest.py
@@ -16,7 +16,7 @@ def main():
 def createScene(root_node):
     root_node.name = "root"
     root_node.dt = 0.1
-    root_node.gravity = [-0.981, 0, 0]
+    root_node.gravity = [0, 0, 0]
 
     plugins = root_node.addChild('plugins')
 
@@ -46,9 +46,9 @@ def createScene(root_node):
     root_node.addObject('VisualStyle', displayFlags="showVisual")
     root_node.addObject('ConstraintAttachButtonSetting')
     root_node.addObject('FreeMotionAnimationLoop', computeBoundingBox=False)
-    root_node.addObject('BlockGaussSeidelConstraintSolver', maxIterations=1000, tolerance=1.0e-6)
+    root_node.addObject('ProjectedGaussSeidelConstraintSolver', maxIterations=1000, tolerance=1.0e-6)
     root_node.addObject('CollisionPipeline', name="Pipeline")
-    root_node.addObject('ParallelBruteForceBroadPhase', name="BroadPhase")
+    root_node.addObject('BruteForceBroadPhase', name="BroadPhase")
     root_node.addObject('ParallelBVHNarrowPhase', name="NarrowPhase")
     root_node.addObject('CollisionResponse', name="ContactManager", response="FrictionContactConstraint", responseParams="mu=0.4")
     root_node.addObject('CCDTightInclusionIntersection', name="Intersection", continuousCollisionType="FreeMotion",maxIterations=1000, contactDistance=0.0)
@@ -60,18 +60,16 @@ def createScene(root_node):
 
     Liver.addObject('EulerImplicitSolver')
     Liver.addObject('SparseLDLSolver', name="ldl", template="CompressedRowSparseMatrixMat3x3", parallelInverseProduct=True)
-    Liver.addObject('MeshGmshLoader', name="meshLoader", filename="mesh/liver.msh", scale3d=[0.2, 0.2, 0.2], rotation=[0, 180, -45], translation=[15.2, -0.15, 0.14])
+    Liver.addObject('MeshGmshLoader', name="meshLoader", filename="mesh/liver.msh", scale3d=[0.2, 0.2, 0.2], rotation=[0, 180, -45], translation=[15.2, 15.65, 15.64])
     Liver.addObject('TetrahedronSetTopologyContainer', name="Container", src='@meshLoader')
     Liver.addObject('TetrahedronSetTopologyModifier', name="Modifier")
-    Liver.addObject('TetrahedronSetGeometryAlgorithms', name="Algorithms")
-    Liver.addObject('MechanicalObject', name="mstate", template="Vec3d", position="@Container.position", velocity=[-5,0,0]*181)
+    Liver.addObject('MechanicalObject', name="mstate", template="Vec3d", position="@Container.position", velocity=[-3,-3,-3]*181)
     Liver.addObject('TetrahedronFEMForceField', name="forceField", listening=True, youngModulus=1e2, poissonRatio=0.3, method="large")
     Liver.addObject('MeshMatrixMass', totalMass=3)
 
     surface = Liver.addChild('Surface')
     surface.addObject('TriangleSetTopologyContainer', name="Container")
     surface.addObject('TriangleSetTopologyModifier', name="Modifier")
-    surface.addObject('TriangleSetGeometryAlgorithms', name="Algorithms")
     surface.addObject('Tetra2TriangleTopologicalMapping', input="@../Container", output="@Container", flipNormals=False)
     surface.addObject('MechanicalObject', name="dofs", rest_position="@../mstate.rest_position")
     surface.addObject('PointCollisionModel', name="Collision", contactDistance=0.0001, color=[0.0, 0.93725490196078, 0.89411764705882, 1])
@@ -80,7 +78,6 @@ def createScene(root_node):
     visual = Liver.addChild('Visual')
     visual.addObject('TriangleSetTopologyContainer', name="Container")
     visual.addObject('TriangleSetTopologyModifier', name="Modifier")
-    visual.addObject('TriangleSetGeometryAlgorithms', name="Algorithms")
     visual.addObject('Tetra2TriangleTopologicalMapping', input="@../Container", output="@Container", flipNormals=False)
     visual.addObject('OglModel', name="VisualModel", src="@Container", color=[0.5, 0, 0.125, 1])
     visual.addObject('IdentityMapping', name="SurfaceMapping")
@@ -106,15 +103,13 @@ def createScene(root_node):
     PlateMecha.addObject('SparseLDLSolver', name="ldl", template="CompressedRowSparseMatrixMat3x3", parallelInverseProduct=True)
     PlateMecha.addObject('TetrahedronSetTopologyContainer', name="Container", position="@../BeamDomainFromGridTopology/HexaTop.position", tetrahedra="@../BeamDomainFromGridTopology/TetraTopology/Container.tetrahedra")
     PlateMecha.addObject('TetrahedronSetTopologyModifier', name="Modifier")
-    PlateMecha.addObject('TetrahedronSetGeometryAlgorithms', name="Algorithms")
-    PlateMecha.addObject('MechanicalObject', name="mstate", template="Vec3d", position="@Container.position", velocity=[15,0,0]*300)
+    PlateMecha.addObject('MechanicalObject', name="mstate", template="Vec3d", position="@Container.position", velocity=[10,10,10]*300)
     PlateMecha.addObject('TetrahedronFEMForceField', name="forceField", listening=True, youngModulus=2e3, poissonRatio=0.3, method="large")
     PlateMecha.addObject('MeshMatrixMass', totalMass=2)
 
     visual = PlateMecha.addChild('Visual')
     visual.addObject('TriangleSetTopologyContainer', name="Container")
     visual.addObject('TriangleSetTopologyModifier', name="Modifier")
-    visual.addObject('TriangleSetGeometryAlgorithms', name="Algorithms")
     visual.addObject('Tetra2TriangleTopologicalMapping', input="@../Container", output="@Container", flipNormals=False)
     visual.addObject('BoxROI', name="FrontCollisionTriangles", box=[0.25, 1.6, 1.6, 0.35, -1.6, -1.6])
     visual.addObject('BoxROI', name="BackCollisionTriangles", box=[-0.35, 1.6, 1.6, -0.25, -1.6, -1.6])
@@ -126,34 +121,13 @@ def createScene(root_node):
     surface = PlateMecha.addChild('FrontSurface')
     surface.addObject('TriangleSetTopologyContainer',triangles="@../Visual/FrontCollisionTriangles.trianglesInROI")
     surface.addObject('TriangleSetTopologyModifier', name="Modifier")
-    surface.addObject('TriangleSetGeometryAlgorithms', name="Algorithms")
     surface.addObject('MechanicalObject', name="dofs", rest_position="@../mstate.rest_position")
-    surface.addObject('TriangleCollisionModel', name="Collision",group=1, contactDistance=0.000001, color=[0.94117647058824, 0.93725490196078, 0.89411764705882, 1])
-    surface.addObject('IdentityMapping', name="SurfaceMapping")
-
-
-
-    surface = PlateMecha.addChild('BackCollisionEdges')
-    surface.addObject('TriangleSetTopologyContainer',triangles="@../Visual/BackCollisionTriangles.trianglesInROI")
-    surface.addObject('TriangleSetTopologyModifier', name="Modifier")
-    surface.addObject('TriangleSetGeometryAlgorithms', name="Algorithms")
-    surface.addObject('MechanicalObject', name="dofs", rest_position="@../mstate.rest_position")
-    surface.addObject('PointCollisionModel', name="Collision", group=1, contactDistance=0.000001, color=[0.94117647058824, 0.93725490196078, 0.89411764705882, 1])
+    surface.addObject('TriangleCollisionModel', name="Collision", contactDistance=0.000001, color=[0.94117647058824, 0.93725490196078, 0.89411764705882, 1])
     surface.addObject('IdentityMapping', name="SurfaceMapping")
 
 
     PlateMecha.addObject('LinearSolverConstraintCorrection', linearSolver="@ldl")
 
-
-
-    #Ground
-    Ground = root_node.addChild('Ground')
-
-    Ground.addObject('TriangleSetTopologyContainer', name="FloorTopology", position="-5 -15 -15  -5 -15 15  -5 15 15  -5 15 -15", triangles="0 2 1  0 3 2")
-    Ground.addObject('MechanicalObject', name="FloorDOF", template="Vec3d")
-    Ground.addObject('TriangleCollisionModel', name="FloorCM", contactDistance="0.0001", moving="0", simulated="0", color="0.3 0.3 0.3 0.1")
-    visu = Ground.addChild('Visu')
-    visu.addObject('OglModel', name="VisualModel", src="@../FloorTopology")
 
 if __name__ == "__main__":
     main()
